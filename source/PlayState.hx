5981f301e1eddf219ccec963b26f32a83ebb3a07
Starmapo <starmapothe@gmail.com>
2022-01-04 12:14:25 -0400

    Convert to lowercase and trim event character types

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -2992,7 +2992,7 @@ class PlayState extends MusicBeatState
 
 			case 'Alt Idle Animation':
 				var char:Character = dad;
-				switch(value1.toLowerCase()) {
+				switch(value1.toLowerCase().trim()) {
 					case 'gf' | 'girlfriend':
 						char = gf;
 					case 'boyfriend' | 'bf':
@@ -3029,7 +3029,7 @@ class PlayState extends MusicBeatState
 
 			case 'Change Character':
 				var charType:Int = 0;
-				switch(value1) {
+				switch(value1.toLowerCase().trim()) {
 					case 'gf' | 'girlfriend':
 						charType = 2;
 					case 'dad' | 'opponent':
