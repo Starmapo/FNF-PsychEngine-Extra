6d4a19901278be4e963cd903a199ded82aa262a8
Starmapo <fabian.xavier7@gmail.com>
2022-02-28 17:12:14 -0400

    idk

--- source/openfl/display/FPS.hx
+++ source/openfl/display/FPS.hx
@@ -1,7 +1,5 @@
 package openfl.display;
 
-import haxe.Timer;
-import openfl.events.Event;
 import openfl.text.TextField;
 import openfl.text.TextFormat;
 import flixel.math.FlxMath;
@@ -11,6 +9,7 @@ import openfl.display._internal.stats.DrawCallContext;
 #end
 #if flash
 import openfl.Lib;
+import openfl.events.Event;
 #end
 
 #if openfl
